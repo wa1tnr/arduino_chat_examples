ee026dbaaf6bdbee27e56a656e2d64e729ac46a9
wa1tnr <wa1tnr@users.noreply.github.com>
2022-02-24 12:36:00 +0000

    saved initial functioning program
    blinks 800 ms on or off

--- series-hh-a.d/blink-pattern-a/n_sketch.cpp
+++ series-hh-a.d/blink-pattern-a/n_sketch.cpp
@@ -1,10 +1,30 @@
-// Thu 24 Feb 11:52:57 UTC 2022
+// Thu 24 Feb 12:33:53 UTC 2022
 #include <Arduino.h>
 
+#define BLUE 1
+#define RED 2
+#define GREEN 3
+#define ORANGE 4
+#define YELLOW 5
+
+void cpl(int PIN) {
+  bool state = digitalRead(PIN);
+  state = !state;
+  digitalWrite(PIN, state);
+}
+
+
 void setup(void) {
+  pinMode(BLUE, 1); // output
+  pinMode(RED, 1);
+  pinMode(GREEN, 1);
+  pinMode(ORANGE, 1);
+  pinMode(YELLOW, 1);
 }
 
 void loop(void) {
+  cpl(BLUE); cpl(RED); cpl(GREEN); cpl(ORANGE); cpl(YELLOW);
+  delay(800);
 }
 
 // END.
