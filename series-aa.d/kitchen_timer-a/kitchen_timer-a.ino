32161c02a7e7cf8ef95226a0863cd2d6e0b520b2
wa1tnr <wa1tnr@users.noreply.github.com>
2022-06-07 12:44:25 +0000

    untested but good online

--- series-aa.d/kitchen_timer-a/kitchen_timer-a.ino
+++ series-aa.d/kitchen_timer-a/kitchen_timer-a.ino
@@ -2,15 +2,25 @@
 
   Tue  7 Jun 12:38:26 UTC 2022
 
-  Hello.
-
 #endif
+
+uint8_t count;
 
 void setup() {
     Serial.begin(9600);
     Serial.println("operational.");
+    count = 0; // base 60 foo idea here
 }
 
-void loop()  {}
+void loop()  {
+  delay(8000); // expecting cognitive issues here ;)
+  count++; // increment
+  if (count > 59) { // base 60 roll-over - modulo-60 foo
+      count = 0;
+  }
+  Serial.print(" count is: ");
+  Serial.print(count);
+  Serial.print("   "); // no carriage return wanted right now.
+}
 
 // END.
