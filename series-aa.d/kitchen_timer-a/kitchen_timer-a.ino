6853f0b413417cb0e895e777841cdd5f32c2269b
wa1tnr <wa1tnr@users.noreply.github.com>
2022-06-07 12:40:02 +0000

    init serial and print hello

--- series-aa.d/kitchen_timer-a/kitchen_timer-a.ino
+++ series-aa.d/kitchen_timer-a/kitchen_timer-a.ino
@@ -1,9 +1,16 @@
 #if 0
 
-  Tue  7 Jun 12:33:26 UTC 2022
+  Tue  7 Jun 12:38:26 UTC 2022
 
   Hello.
 
 #endif
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("operational.");
+}
+
+void loop()  {}
 
 // END.
