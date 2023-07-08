0c376a6ef3ff57dea18eab73fe346b76f63906c8
wa1tnr <wa1tnr@users.noreply.github.com>
2023-07-08 18:05:15 +0000

    sync changes all over
    	modified:   sample_run.log
    	modified:   test.fs
    
    On branch dvlp-aa

--- series-jj-a.d/gforth/multi-char-a/test.fs
+++ series-jj-a.d/gforth/multi-char-a/test.fs
@@ -9,15 +9,15 @@
 \ = 'abcd' = (('a'*256 + 'b')*256 + 'c')*256 + 'd'
 
     : ndefaa [char] a 256 * ; CR
-                                               48 spaces ." char a ." space char a . CR
+                                        48 spaces ." char a  ." space char a . ."  ok" CR
 ."  : ndefaa [char] a 256 * ;"     12 spaces ndefaa . 4 spaces
-                                                         ." char b ." space char b . CR
+                                                  ." char b  ." space char b . ."  ok" CR
     : ndefab [char] b ndefaa + ;
 ."  : ndefab [char] b ndefaa + ;"   9 spaces ndefab . CR
     : ndefac ndefab 256 * ;
 ."  : ndefac ndefab 256 * ;"       12 spaces ndefac . 4 spaces
     : ndefad ndefac [char] c + ;
-                                                              ." char c ." char c . CR
+                                                  ." char c  ." space char c . ."  ok" CR
 
 \ = 'abcd' = (('a'*256 + 'b')*256 + 'c')*256 + 'd'
 
@@ -25,7 +25,7 @@
     : ndefae ndefad 256 * ;
 ."  : ndefae ndefad 256 * ;"        9 spaces ndefae . 4 spaces
     : ndefaf ndefae [char] d + ;
-                                                              ." char d ." char d . CR
+                                                  ." char d ." space char d . ."  ok" CR
 ."  : ndefaf ndefae [char] d + ;"   4 spaces ndefaf . 4 spaces CR
 
 33 spaces ." 1633837924 = 0x61626364 = '0xa0xb0xc0xd'" CR
@@ -38,5 +38,5 @@ CR
 CR
 ." famnextdo " CR
 
-\ Sat  8 Jul 17:30:43 UTC 2023
+." Sat  8 Jul 17:57:11 UTC 2023" CR
 \ END.
