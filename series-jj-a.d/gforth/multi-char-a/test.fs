21a53626aa051c963d4d67e254a3e04e533e4c8d
wa1tnr <wa1tnr@users.noreply.github.com>
2023-07-08 17:31:50 +0000

    finished program done

--- series-jj-a.d/gforth/multi-char-a/test.fs
+++ series-jj-a.d/gforth/multi-char-a/test.fs
@@ -9,25 +9,34 @@
 \ = 'abcd' = (('a'*256 + 'b')*256 + 'c')*256 + 'd'
 
     : ndefaa [char] a 256 * ; CR
+                                               48 spaces ." char a ." space char a . CR
 ."  : ndefaa [char] a 256 * ;"     12 spaces ndefaa . 4 spaces
-." char b ." char b . CR
+                                                         ." char b ." space char b . CR
     : ndefab [char] b ndefaa + ;
 ."  : ndefab [char] b ndefaa + ;"   9 spaces ndefab . CR
     : ndefac ndefab 256 * ;
 ."  : ndefac ndefab 256 * ;"       12 spaces ndefac . 4 spaces
     : ndefad ndefac [char] c + ;
+                                                              ." char c ." char c . CR
 
-." char c ." char c . CR
+\ = 'abcd' = (('a'*256 + 'b')*256 + 'c')*256 + 'd'
 
-."  : ndefad ndefac [char] c + ;"  7 spaces ndefad . CR
+."  : ndefad ndefac [char] c + ;"   7 spaces ndefad . 4 spaces CR
+    : ndefae ndefad 256 * ;
+."  : ndefae ndefad 256 * ;"        9 spaces ndefae . 4 spaces
+    : ndefaf ndefae [char] d + ;
+                                                              ." char d ." char d . CR
+."  : ndefaf ndefae [char] d + ;"   4 spaces ndefaf . 4 spaces CR
 
+33 spaces ." 1633837924 = 0x61626364 = '0xa0xb0xc0xd'" CR
 CR
 
 
 0 [if]
     ERRATA = 'abcd' = (('a'*256 + 'b')*256 + `c`)*256 + 'd'
 [then]
-
+CR
 ." famnextdo " CR
 
+\ Sat  8 Jul 17:30:43 UTC 2023
 \ END.
